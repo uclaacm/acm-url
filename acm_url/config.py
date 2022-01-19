b147fdfe1c067ab1904ae7525efe4f8e7ff6a5ce
Ellie Cheng <ellieyhc@gmail.com>
2022-01-19 11:25:15 -0800

    Revert "Merge pull request #16 from uclaacm/all-page"
    This reverts commit 65d578f8b88f65d0e3143a4c2c4a4dc2f63d882b, reversing
    changes made to e2aa9c801b6e8d5f7d0dd49f8072e92edd79ec3a.

--- acm_url/config.py
+++ acm_url/config.py
@@ -4,4 +4,3 @@ class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev'
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'acm_url.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTS_PER_PAGE=10
