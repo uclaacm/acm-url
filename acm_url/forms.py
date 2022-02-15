681eae89e7a54259220cb1cb4fda2b1c60bfe596
Victor Wen <vicwen02@g.ucla.edu>
2022-02-14 21:12:59 -0800

    Added edit.html and refactored /edit route to use flask form

--- acm_url/forms.py
+++ acm_url/forms.py
@@ -7,6 +7,11 @@ class CreateForm(FlaskForm):
   url = StringField('Enter long URL to make short', validators=[DataRequired(message="A URL was not entered.")])
   submit = SubmitField('Make URL')
 
+class EditForm(FlaskForm):
+  vanity = StringField('Custom vanity', validators=[Regexp('^[\w-]+$', message="Short name must only containt letters, digits, and dashes.")])
+  url = StringField('Enter new URL for this vanity', validators=[DataRequired(message="A URL was not entered.")])
+  submit = SubmitField('Edit URL')
+
 class PasswordForm(FlaskForm):
   password = PasswordField('Password', validators=[InputRequired(message="You must submit the password to create an URL.")])
   submit = SubmitField('Submit')
--- acm_url/routes.py
+++ acm_url/routes.py
@@ -4,7 +4,7 @@ import string
 import secrets
 import re
 import os
-from acm_url.forms import CreateForm, PasswordForm
+from acm_url.forms import CreateForm, PasswordForm, EditForm
 from werkzeug.security import check_password_hash
 from acm_url.schema import URL
 from sqlalchemy import func
@@ -112,25 +112,27 @@ def delete(vanity):
     db.session.commit()
     return redirect(url_for('all'))
 
-@app.route('/edit', methods=['POST'])
+@app.route('/edit', methods=['GET','POST'])
 def edit():
-    # Grab request data
-    vanity = request.form['vanity']
-    url = request.form['url']
+    edit_form = EditForm()
 
-    # Check if the vanity is valid
-    if is_unavaliable(vanity):
-        return f"You cannot use the short name {vanity}", 405
+    if edit_form.validate_on_submit():
+        vanity = edit_form.vanity.data
+        url = edit_form.url.data
 
-    # Check if vanity exists
-    entry = URL.query.filter(func.lower(URL.vanity) == func.lower(vanity)).first()
-    if entry is None:
-        return f"No vanity found with name {vanity}", 404
+        # Check if the vanity is valid
+        if is_unavaliable(vanity):
+            return render_template("404.html")
+        # Check if vanity exists
+        entry = URL.query.filter(func.lower(URL.vanity) == func.lower(vanity)).first()
+        if entry is None:
+            return render_template('404.html')
 
-    # Update database
-    if not(url.startswith('http://') or url.startswith('https://')):
-        url = 'https://' + url
-    entry.url = url
-    db.session.commit()
+        # Update database
+        if not(url.startswith('http://') or url.startswith('https://')):
+            url = 'https://' + url
+        entry.url = url
+        db.session.commit()
+        return redirect(url_for('all'))
 
-    return "Complete", 200
+    return render_template('edit.html', form=edit_form)
